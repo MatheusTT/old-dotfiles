6514a2fb23bea2d0c4cb0c75672e7e3ac2ffec5f
MatheusTT <matheusttomaz75@gmail.com>
2022-05-22 16:30:04 -0300

    Added my script to make spotify to got his workspace, that i forgot

--- install.sh
+++ install.sh
@@ -6,38 +6,43 @@ dotfiles_path="$( dirname "${BASH_SOURCE[0]}" )"
 # install LightDM and use my config with it.
 install_lightdm=true
 
-# installing an AUR helper
+# Small script made to redirect Spotify to
+# a desktop, because bspc rule doesn't work
+# with Spotify
+install_spotify_workaround=true
+
+# Installing an AUR helper
 if ( ! pacman -Qs "^paru" 1>/dev/null ) ; then
     cd /tmp && git clone --depth=1 https://aur.archlinux.org/paru.git 1>/dev/null
     cd paru && makepkg -sic --noconfirm 1>/dev/null
 fi
 
-# installing programs
+# Installing programs
 sudo pacman -S --needed --noconfirm xss-lock kitty rofi feh redshift playerctl pulsemixer dunst \
   flameshot polkit-gnome brightnessctl numlockx xorg-{setxkbmap,xset,xsetroot} \
   nodejs-material-design-icons ttf-jetbrains-mono ttf-fira-{code,mono,sans} wget bat btop \
   bspwm sxhkd polybar ranger papirus-icon-theme cmatrix neofetch typespeed libpulse xclip yt-dlp lsd
 
 paru -S --needed --noconfirm i3lock-color-git zscroll-git picom-jonaburg-git nerd-fonts-{jetbrains-mono,fira-code} \
-  xcursor-breeze pipes.sh rxfetch pfetch thokr-git ipman
+  xcursor-breeze pipes.sh rxfetch pfetch thokr-git ipman spotify
 
 
-# installing oh my zsh and the plugins
-# Oh My Zsh
+# Installing oh my zsh and the plugins
+## Oh My Zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
-# Plugins
+## Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# installing NvChad
+# Installing NvChad
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
 
-# installing ranger devicons
+# Installing ranger devicons
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
-# linking files
+# Linking files
 paths=(
   .config/BetterDiscord
   .config/bspwm
@@ -63,15 +68,24 @@ for i in ${paths[@]} ; do
   ln -sf $dotfiles_path/$i ~/$i 
 done
 
-# NvChad custom/chadrc.lua and TokyoNight GTK theme 
+
+## NvChad custom dir
 ln -sf $dotfiles_path/.config/nvim/lua/custom ~/.config/nvim/lua/custom
 
+## TokyoNight GTK theme
 temp_tokyo_path="/tmp/tokyo-gtk"
 git clone https://github.com/stronk-dev/Tokyo-Night-Linux.git $temp_tokyo_path
 sudo cp -r $temp_tokyo_path/usr/share/themes/TokyoNight /usr/share/themes/TokyoNight
 rm -rf $temp_tokyo_path
 
-# configuring lightdm-gtk-greeter
+## Spotify Workaround
+if ( $install_spotify_workaround ); then
+  ln -f .local/bin/spotify-workaround ~/.local/bin/spotify-workaround
+  ln -f .local/share/applications/spotify.desktop ~/.local/share/applications/spotify.desktop
+fi
+
+
+## Configuring lightdm-gtk-greeter
 if ( $install_lightdm ); then
   sudo pacman -S lightdm-gtk-greeter-settings
   sudo systemctl enable lightdm
